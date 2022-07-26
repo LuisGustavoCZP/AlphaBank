5e7071d9d42905e9f668a1db1fe8d4d5c8a90776
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-07-26 17:32:46 -0300

    server multipages serve

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -8,11 +8,15 @@ const app: Express = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static("../web/dist"));
+//app.use("/assets", express.static("../web/dist/assets"));
+app.use("/", express.static("../web/dist"));
 
 app.use("/ping", ping);
 //app.use("/user", user);
 app.use("/account", account);
 //app.use("/transaction")
-app.use(transaction)
+app.use(transaction);
+
+app.use("/:page", express.static("../web/dist/index.html"));
+
 export default app;
